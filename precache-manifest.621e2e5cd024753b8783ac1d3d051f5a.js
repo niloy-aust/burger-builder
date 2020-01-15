a12f5261e271322673f0b8868cefc70db9822249
Niloy Saha <niloyaust012@gmail.com>
2020-01-15 21:55:54 +0600

    Updates

---
+++ precache-manifest.621e2e5cd024753b8783ac1d3d051f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46d481690ff0b0f337be0e0f436fb00",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "f431a65aced60bfb3750",
+    "url": "/burger-builder/static/css/main.e5d2a728.chunk.css"
+  },
+  {
+    "revision": "617bfa985e948911abc1",
+    "url": "/burger-builder/static/js/2.61dcd27c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/burger-builder/static/js/2.61dcd27c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f431a65aced60bfb3750",
+    "url": "/burger-builder/static/js/main.1ae44d20.chunk.js"
+  },
+  {
+    "revision": "116f80cde4ba79b87dd4",
+    "url": "/burger-builder/static/js/runtime-main.bb377053.js"
+  }
+]);
